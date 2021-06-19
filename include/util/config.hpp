52f1c65b1a8b1d35921b6c005775ea1b67f1130b
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2021-06-20 02:38:04 +0800

    V1.4.3 (Part 2)

--- include/util/config.hpp
+++ include/util/config.hpp
@@ -3,9 +3,9 @@
 #include <vector>
 
 namespace inst::config {
-    static const std::string appDir = "sdmc:/switch/AtmoXL-Titel-Installer";
+    static const std::string appDir = "sdmc:/config/AtmoXL-Titel-Installer";
     static const std::string configPath = appDir + "/config.json";
-    static const std::string appVersion = "1.4.2";
+    static const std::string appVersion = "1.4.3";
 
     extern std::string gAuthKey;
     extern std::string lastNetUrl;
