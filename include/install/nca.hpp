55f75bca62716eccab01c77666c1fc0a13ea40d7
R-YaTian <47445484+R-YaTian@users.noreply.github.com>
2021-09-26 16:38:13 +0800

    Minor update

--- include/install/nca.hpp
+++ include/install/nca.hpp
@@ -8,8 +8,7 @@ namespace tin::install
 {
     struct NcaFsHeader
     {
-        u8 _0x0;
-        u8 _0x1;
+        u16 version;
         u8 partition_type;
         u8 fs_type;
         u8 crypt_type;
@@ -28,7 +27,8 @@ namespace tin::install
                 u32 section_ctr_high;
             };
         };
-        u8 _0x148[0xB8]; /* Padding. */
+        u8 sparse_info[0x30]; /* only used in sections with sparse storage. */
+        u8 _0x178[0x88]; /* Padding. */
     } PACKED;
 
     static_assert(sizeof(NcaFsHeader) == 0x200, "NcaFsHeader must be 0x200");
@@ -53,7 +53,7 @@ namespace tin::install
         u8 m_kaekIndex; /* Which kaek index? */
         u64 nca_size; /* Entire archive size. */
         u64 m_titleId;
-        u8 _0x218[0x4]; /* Padding. */
+        u32 m_contentIndex;
         union {
             uint32_t sdk_version; /* What SDK was this built with? */
             struct {
@@ -64,7 +64,8 @@ namespace tin::install
             };
         };
         u8 m_cryptoType2; /* Which keyblob (field 2) */
-        u8 _0x221[0xF]; /* Padding. */
+        u8 m_cryptoType3;
+        u8 _0x222[0xE]; /* Padding. */
         u64 m_rightsId[2]; /* Rights ID (for titlekey crypto). */
         NcaSectionEntry section_entries[4]; /* Section entry metadata. */
         u8 section_hashes[4 * 0x20]; /* SHA-256 hashes for each section header. */
